eb389878f72d554980dac0f48ce3cb51e13675b4
jossseluuu <155114962+jossseluuu@users.noreply.github.com>
2024-10-06 20:08:55 +0200

    Iniciada la base de la clase Trigo. Closes #1 Asignados los atributos de instancia y de la clase Trigo. Closas #2 Creados los métodos getters y settes de la clase Trigo. Closes #3 Implementados los constructores (3) de la clase Trigo. Closes #4

--- MyHayDay/src/myhayday/Trigo.java
+++ MyHayDay/src/myhayday/Trigo.java
@@ -4,10 +4,88 @@
  */
 package myhayday;
 
-/**
- *
+import java.util.HashSet;
+ 
+/*
  * @author josel
- */
+*/
 public class Trigo {
+    // --------------------------------------- //
+    //                Atributos                //
+    // --------------------------------------- //
+    
+    public static final String ICONO = "/img/trigo.png";
+    public static final int DURACION = 2;
+    public static final int EXPERIENCIA = 1;
+    public static final int PRECIO_VENTA = 2;
+    public static final int RIEGO = 10;
+    
+    private String icono;
+    private int duracionProduccion;
+    private int experienciaRecolecta;
+    private int precioVenta;
+    private int riego;
+    
+    // --------------------------------------- //
+    //               Constructor               //
+    // --------------------------------------- //
+    
+    //Constructor Paramétrico
+    public Trigo(String icono, int duracionProduccion, int experienciaRecolecta, int precioVenta, int riego){
+        this.icono = icono;
+        this.duracionProduccion = duracionProduccion;
+        this.experienciaRecolecta = experienciaRecolecta;
+        this.precioVenta = precioVenta;
+        this.riego = riego;
+    }
+    //Constructor Vacío
+    public Trigo(){
+        this(ICONO, DURACION, EXPERIENCIA, PRECIO_VENTA, RIEGO);
+    }
+    //Constructor Copia
+    public Trigo(Trigo otro){
+        this(otro.icono, otro.duracionProduccion, otro.experienciaRecolecta, otro.precioVenta, otro.riego);
+    }
+    
+    // --------------------------------------- //
+    //            Getters y Setters            //
+    // --------------------------------------- //
+    
+    //Metodos Set
+    public void setAll(){
+    }
+    public void setIcono(String icono){
+        this.icono = icono;
+    }
+    public void setDuracionProduccion(int duracionProduccion){
+        this.duracionProduccion = duracionProduccion;
+    }
+    public void setExperienciaRecolecta(int experienciaRecolecta){
+        this.experienciaRecolecta = experienciaRecolecta;
+    }
+    public void setPrecioVenta(int precioVenta){
+        this.precioVenta = precioVenta;
+    }
+    public void setRiego(int riego){
+        this.riego = riego;
+    }
+    //Metodos Get
+    public void getAll(){
+    }
+    public String getIcono(String icono){
+        return icono;
+    }
+    public int getDuracionProduccion(int duracionProduccion){
+        return duracionProduccion;
+    }
+    public int getExperienciaRecolecta(int experienciaRecolecta){
+        return experienciaRecolecta;
+    }
+    public int getPrecioVenta(int precioVenta){
+        return precioVenta;
+    }
+    public int getRiego(int riego){
+        return riego;
+    }
     
 }
